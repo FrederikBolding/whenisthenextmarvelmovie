dc86b02601bfaa0c48ee195ce802dcd40fc73bc8
Frederik Bolding <frederik.bolding@gmail.com>
2018-10-18 23:35:13 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Countdown from './Countdown.js';
+
+function Page(props) {
+  if (props.state !== null) {
+    return (
+      <div className="App">
+        <header className="App-header" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${props.state.bg})`, backgroundPosition: "center", }}>
+          <h1>{props.state.title}</h1>
+          <Countdown date={props.state.date}></Countdown>
+        </header>
+      </div >);
+  } else {
+    return (
+      <div className="App" style={{}}>
+        <header className="App-header">
+          <h1>Loading</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  componentDidMount() {
+    var self = this;
+    fetch("api/nextmovie", {
+    }).then(function (response) {
+      return response.json();
+    }).then(function(movie){
+      self.setState({ title: movie.title, date: movie.release_date, bg: movie.backdrop_path });
+    });
+  }
+
+  render() {
+    return (
+      <Page state={this.state}></Page>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production"||true) {
+  app.use(express.static("client/build"));
+}
+
+
+var tmdbclient = require('themoviedbclient');
+var tmdb = new tmdbclient(process.env.API_KEY);
+
+var nextMovies = [299537, 299534, 429617]
+
+app.get("/api/nextmovie", (req, res) => {
+  getNextMovie(0);
+
+  function getNextMovie(index) {
+    tmdb.call("/movie/" + nextMovies[index], {})
+      .then(function (movie) {
+        var date = new Date(movie.release_date);
+        if (date > new Date()) {
+          res.json(movie);
+          return;
+        } else if (index < nextMovies.length) {
+          getNextMovie(index + 1);
+        }
+      });
+  }
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+});
