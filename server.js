38f1ea18017afa897c47acda9f95ed965fa12c2c
Frederik Bolding <frederik.bolding@gmail.com>
2018-10-20 13:30:36 +0200

    Removed testing condition

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === "production"||true) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
