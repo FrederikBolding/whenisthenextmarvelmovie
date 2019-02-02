b6d422b67250f8d62a09c8baefb2447bd05fcfee
Frederik Bolding <frederik.bolding@gmail.com>
2019-02-03 00:50:13 +0100

    Added small byline at the bottom of the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Countdown from './Countdown.js';
+import MadeWithLove from './MadeWithLove.js'
 
 function Page(props) {
   if (props.state !== null) {
     return (
-      <div className="App">
-        <header className="App-header" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${props.state.bg})`, backgroundSize: "cover", }}>
+      <div className="App" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${props.state.bg})`, backgroundSize: "cover", }}>
+        <header className="App-header">
           <h1>{props.state.title}</h1>
           <Countdown date={props.state.date}></Countdown>
+          <MadeWithLove by="Frederik Bolding" link="https://frederik.bolding.com" />
         </header>
       </div >);
   } else {
@@ -16,6 +18,7 @@ function Page(props) {
       <div className="App" style={{}}>
         <header className="App-header">
           <h1>Loading</h1>
+          <MadeWithLove by="Frederik Bolding" link="https://frederik.bolding.com" />
         </header>
       </div>
     );
