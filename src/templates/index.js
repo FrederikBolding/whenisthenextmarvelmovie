7516e21f24de0f666ca22f11e50aa4c7da451e25
Frederik Bolding <frederik.bolding@gmail.com>
2019-03-10 18:18:20 +0100

    Centered bg image vertically

--- src/templates/index.js
+++ src/templates/index.js
@@ -9,8 +9,8 @@ import "../components/index.css"
 
 const IndexPage = ({ pageContext: { nextMovie } }) => (
   <Layout>
-    <SEO title="When is the next Marvel movie?" keywords={[`gatsby`, `application`, `react`, `marvel`]} />
-    <div className="App" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${nextMovie.backdrop_path})`, backgroundSize: "cover" }}>
+    <SEO title="When is the next Marvel movie?" keywords={[`gatsby`, `application`, `react`, `marvel`, `superheroes`]} />
+    <div className="App" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${nextMovie.backdrop_path})`, backgroundSize: "cover", backgroundPositionX: "center" }}>
         <header className="App-header">
           <h1>{nextMovie.title}</h1>
           <Countdown date={nextMovie.release_date}></Countdown>
