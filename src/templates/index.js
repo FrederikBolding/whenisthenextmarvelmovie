0056aca0c0b5ef8870333083050664434bc92076
Frederik Bolding <frederik.bolding@gmail.com>
2019-05-24 01:05:18 +0200

    Simplified code/structure a bit

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Countdown from '../components/Countdown.js';
 import MadeWithLove from '../components/MadeWithLove.js'
@@ -8,16 +7,16 @@ import MadeWithLove from '../components/MadeWithLove.js'
 import "../components/index.css"
 
 const IndexPage = ({ pageContext: { nextMovie } }) => (
-  <Layout>
+  <main>
     <SEO title="When is the next Marvel movie?" keywords={[`gatsby`, `application`, `react`, `marvel`, `superheroes`]} />
     <div className="App" style={{ backgroundImage: `url(http://image.tmdb.org/t/p/original/${nextMovie.backdrop_path})`, backgroundSize: "cover", backgroundPositionX: "center" }}>
-        <header className="App-header">
+        <div className="App-content">
           <h1>{nextMovie.title}</h1>
           <Countdown date={nextMovie.release_date}></Countdown>
           <MadeWithLove by="Frederik Bolding" link="https://frederikbolding.com" />
-        </header>
+        </div>
       </div>
-  </Layout>
+  </main>
 )
 
 export default IndexPage
